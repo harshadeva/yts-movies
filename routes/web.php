211dc19230cd3b3c188831e90d3442a388a88e1a
Harshadeva <harshadeva@webmotech.com>
2024-10-27 15:56:17 +0530

    hosted

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,16 @@
 <?php
 
 use App\Http\Controllers\MovieController;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/commands', function () {
+    Artisan::call('migrate');
+    return 'executed';
 });
 
 Route::get('/movies',[MovieController::class,'render']);
